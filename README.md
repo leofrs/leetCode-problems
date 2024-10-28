b97eb7d83035ae9a97dfdb321ea9467f56fbf6aa
Leo Santos <leofrrodrigues86@gmail.com>
2024-10-28 08:55:41 -0300

    Questão 3 adicionada e resolvida

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 1 - Dado um array de tamanho n determine se existem valores duplicados. Caso SIM retorno `true` no console, caso contrario, retorne `false`.
 
-2 - Dado um array de `números` com tamanho `n` no range de `[0,n]`, retorne o unico número dentro desse range que está falatando
+2 - Dado um array de `números` com tamanho `n` no range de `[0,n]`, retorne o unico número dentro desse range que está faltando
+
+3 - Give an array nums of n integers where nums[i] is in the range [1,n], return an array of all the integers in the range [1,n] that do not appear in nums.
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ for (let i = 0; i < array.length; i++) {
  */
 
 // Questão 2
-const array = [1, 2, 4, 5]; // Aqui está faltando o número 3
+/* const array = [1, 2, 4, 5]; // Aqui está faltando o número 3
 
 const n = 5;
 
@@ -29,4 +29,28 @@ const arrayAtual = array.reduce((acc, current) => {
 
 const valorFaltando = arrayCompleto - arrayAtual;
 
-console.log(`O valor que está faltando é o número ${valorFaltando}`);
+console.log(`O valor que está faltando é o número ${valorFaltando}`); */
+
+// Questão 3
+/* const array = [4, 3, 2, 7, 8, 2, 3, 1]; //Faltando o [5,6]
+const n = array.length;
+
+const presence = new Array(n + 1).fill(false);
+
+for (const num of array) {
+	if (num >= 1 && num <= n) {
+		presence[num] = true;
+	}
+}
+
+console.log(presence);
+
+const missingNumbers = [];
+
+for (let i = 1; i <= n; i++) {
+	if (!presence[i]) {
+		missingNumbers.push(i);
+	}
+}
+
+console.log(missingNumbers); */
